f2e0a1a87839b63b3bd5d468057f4475b016c197
“RoOtT24” <1190579@student.birzeit.edu>
2023-06-20 16:35:30 +0300

    apikey .env

--- src/Services/cloudinary.js
+++ src/Services/cloudinary.js
@@ -2,6 +2,6 @@ import cloudinary from 'cloudinary';
 
 cloudinary.config({ 
     cloud_name: 'dfulwmdbb', 
-    api_key: '117893371571512', 
-    api_secret: 'yiJYYFb0lRpxbzVRSDNBSvXS_c0' 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET 
   });
